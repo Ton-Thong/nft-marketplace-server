3e553cdaf69886a404077ae52a1d66cdf5d3bc72
Ton-Thong <trakulparn@hotmail.com>
2022-01-19 00:22:21 +0700

    remove truffle

--- migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const RuNFT = artifacts.require('RuNFT');
-const NFTMarket = artifacts.require('NFTMarket');
-
-module.exports = function (deployer) {
-  deployer.deploy(RuNFT);
-  deployer.deploy(NFTMarket);
-};
